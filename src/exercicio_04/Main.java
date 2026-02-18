49816dc7fa98d1d63a62ab4effb175840f5e11ae
Aluno Git Davi <davicarneiro.dev@gmail.com>
2026-02-18 02:38:06 -0300

    Finalização exercicio 4

---
+++ src/exercicio_04/Main.java
@@ -0,0 +1,23 @@
+package exercicio_04;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double salario=800;
+        double desc5 =900.00;
+        double desc10 =1500.00;
+        double desc20 =2500.00;
+
+        if (salario <= desc5) {
+            System.out.println("Seu salário de "+salario+" está isento de desconto.");
+        } else if (salario <= desc10) {
+            System.out.println("Seu salário de "+salario+" terá 5% de desconto.");
+        } else if (salario <= desc20) {
+            System.out.println("Seu salário de "+salario+" terá 10% de desconto.");
+        } else {
+            System.out.println("Seu salário de "+salario+" terá 20% de desconto.");
+        }
+
+
+    }
+}
